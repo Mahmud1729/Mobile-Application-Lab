a3137ed276b35e3222a50b3567242869383e4448
Mahmud1729 <2001037@iot.bdu.ac.bd>
2025-10-29 00:26:26 +0600

    Applying add method on an empty string list

--- main.dart
+++ main.dart
@@ -1,4 +1,18 @@
 void main() {
-  Set<String> fruits = {"Orange", "Mango", "Apple"};
-  print(fruits);
+  // creating a empty list of type string
+  List<String> days = [];
+
+  // adding names of 7 days
+  days.add("Sunday");
+  days.add("Monday");
+  days.add("Tuesday");
+  days.add("Wednesday");
+  days.add("Thrusday");
+  days.add("Friday");
+  days.add("Saturday");
+
+  // print all days
+  for (String day in days) {
+    print(day);
+  }
 }
