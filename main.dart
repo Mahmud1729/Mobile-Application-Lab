1ae579f0ea531ffe92d8fc33cc9fa6f4d87cbc9d
Mahmud1729 <2001037@iot.bdu.ac.bd>
2025-10-29 00:54:46 +0600

    Intersection method in Set

--- main.dart
+++ main.dart
@@ -1,10 +1,15 @@
-// Difference in Set
+// Difference and intersections in Set
 
 void main() {
   // declaring fruits as set
   Set<String> fruits1 = {"Orange", "Mango", "Apple"};
   Set<String> fruits2 = {"Apple", "Grapes", "Bannana"};
 
+  // difference method
   final differnceSet = fruits1.difference(fruits2);
   print(differnceSet); //{Orange, Mango}
+
+  // intersection method
+  final intersectionSet = fruits1.intersection(fruits2);
+  print(intersectionSet); //{Apple}
 }
