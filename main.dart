295b3a62321bd3ef79f347c85b79536d6729c965
Mahmud1729 <2001037@iot.bdu.ac.bd>
2025-10-29 00:37:09 +0600

    Applying Set properties in Dart

--- main.dart
+++ main.dart
@@ -1,18 +1,11 @@
 void main() {
-  // creating a empty list of type string
-  List<String> days = [];
+  // declaring fruits as set
+  Set<String> fruits = {"Grapes", "Bannana", "Orange", "Mango", "Apple"};
 
-  // adding names of 7 days
-  days.add("Sunday");
-  days.add("Monday");
-  days.add("Tuesday");
-  days.add("Wednesday");
-  days.add("Thrusday");
-  days.add("Friday");
-  days.add("Saturday");
-
-  // print all days
-  for (String day in days) {
-    print(day);
-  }
+  // Using different properties of Set
+  print("First value is ${fruits.first}");
+  print("Last value is ${fruits.last}");
+  print("Is fruits Empty? ${fruits.isEmpty}");
+  print("Is fruits not Empty ${fruits.isNotEmpty}");
+  print("The length of fruits is ${fruits.length}");
 }
