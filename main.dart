87c80260fea4bfc869bb10b90c0b3d30a4f88f54
Mahmud1729 <2001037@iot.bdu.ac.bd>
2025-11-01 23:38:44 +0600

    Where filter

--- main.dart
+++ main.dart
@@ -1,14 +1,14 @@
 // Applying map methods
 
 void main() {
-  Map<String, dynamic> books = {
-    'Title': "Opekkha",
-    'Author': "Humayun Ahmed",
-    'Pages': 220
+  Map<String, double> mathMarks = {
+    'Mark': 35,
+    'Harry': 42,
+    'Smith': 65,
+    'Warner': 32
   };
 
-  // Loop through map
-  for (MapEntry book in books.entries) {
-    print("Key is ${book.key} Value is ${book.value}");
-  }
+  // Where filter
+  mathMarks.removeWhere((Key, value) => value < 37);
+  print(mathMarks);
 }
